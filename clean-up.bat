be1e639babf872872b3dd78a8a9d87b949c2da8d
Leo K <lkuznetsov@zspace.com>
2017-05-18 18:50:55 -0700

    Work in Progress: msvc2015, msvc2017 build with three additional disabled warnings: 4457: declaration of ... hides function parameter 4458: declaration of ... hides class member 4459: declaration of ... hides global declaration That needs to be eventually enabled and thoroughly investigated...

--- clean-up.bat
+++ clean-up.bat
@@ -1,8 +1,27 @@
-rem @echo off
+@echo off
 rm -rf bin
 rm -rf build
 rm -rf lib
 rm -rf obj
+
+pushd msvc2012
+find . -name "*.sdf"  | xargs rm -f
+find . -name "ipch"   | xargs rm -f
+find . -name "*.suo"  | xargs rm -f
+find . -name "*.pdb"  | xargs rm -f
+find . -name "*.user" | xargs rm -f
+popd
+
+pushd msvc2015
+rm -rf .vs
+find . -name "*.sdf"  | xargs rm -f
+find . -name "ipch"   | xargs rm -f
+find . -name "*.suo"  | xargs rm -f
+find . -name "*.pdb"  | xargs rm -f
+find . -name "*.user" | xargs rm -f
+find . -name "*.VC.db" | xargs rm -f
+popd
+
 pushd msvc2017
 rm -rf .vs
 find . -name "*.sdf"  | xargs rm -f
